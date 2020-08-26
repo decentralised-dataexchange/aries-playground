087d6372873a82ea62c724a70b63c9d531c5921d
George J Padayatti <georgejpadayatty0000@gmail.com>
2020-08-26 21:51:07 +0530

    Exposing cloudagent port for development

---
+++ cloud-agent/startup_temp.sh
@@ -0,0 +1,12 @@
+aca-py start \
+    -it http 0.0.0.0 80 \
+    -ot http \
+    -e $ACAPY_ENDPOINT \
+    --label $AGENT_NAME \
+    --admin 0.0.0.0 8001 \
+    --admin-insecure-mode \
+    --auto-accept-requests --auto-ping-connection \
+    --genesis-url $GENESIS_URL \
+    --wallet-type indy \
+    --wallet-name $AGENT_NAME \
+    --webhook-url $WEBHOOK_URL
